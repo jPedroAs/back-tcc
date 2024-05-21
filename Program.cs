04f552299e90093ca535c55f69ed6135c1e8d840
joao pedro <joaopedroa2120@outlook.com>
2024-05-21 18:41:36 -0300

    kk

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using ApiBalada.Services;
 using JtTcc;
 // using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var key = Encoding.ASCII.GetBytes(Settings.Secret);
@@ -34,10 +35,16 @@ builder.Services.AddDbContext<Context>();
 builder.Services.AddControllers();
 builder.Services.AddScoped<Context>();
 builder.Services.AddTransient<TokenService>();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Nome da Sua API", Version = "v1" });
+    });
 
 var app = builder.Build();
 app.UseCors("AllowAllOrigins");
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 // app.UseAuthentication();
 // app.UseAuthorization();
 
